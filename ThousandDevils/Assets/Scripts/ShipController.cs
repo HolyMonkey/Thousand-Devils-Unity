4d06bd32c08d126ff9f12e7d8113ce164053dc50
Eugene Ptashko <eugene.ptashko@gmail.com>
2019-02-21 15:29:54 +0500

    начал делать контроллер, управляющий передвижением корабля

---
+++ ThousandDevils/Assets/Scripts/ShipController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] private Vector2 shipSpeed;
+
+    [SerializeField] private float shipAngularSpeed;
+
+    [SerializeField] private Vector2 windSpeed;
+
+    [SerializeField] private Vector2 flowSpeed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipSpeed = new Vector2(0, 100f);
+        shipAngularSpeed = 800f;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = shipSpeed * Time.deltaTime;
+
+        rb.angularVelocity = -shipAngularSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+    }
+}
