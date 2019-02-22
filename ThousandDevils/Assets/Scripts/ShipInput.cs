51158aa8e12ae5d0419b1cc9c6a7a89361070617
ShamanJD <eugene.ptashko@gmail.com>
2019-02-22 19:55:39 +0500

    добавил компонент, которыйуправляет кораблем учитывая пользовательский ввод

---
+++ ThousandDevils/Assets/Scripts/ShipInput.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class ShipInput : MonoBehaviour
+{
+    [SerializeField] private GameObject _sheep;
+
+    void Update()
+    {
+        var shipMovementController = _sheep.GetComponent<ShipMovementController>();
+
+        if (Input.GetKeyDown(KeyCode.E))
+            shipMovementController.ToggleSailRaised();
+
+        shipMovementController.SetTurnDirection(Input.GetAxis("Horizontal"));
+    }
+}
