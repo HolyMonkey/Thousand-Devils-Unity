e556182179c1d78715c3f4fa50448cc82bb6e69a
Дмитрий Дудин <empire_95@mail.ru>
2019-03-04 23:04:39 +0300

    Создание зон с течением и ветром, которые увеличивают скорость игрока

---
+++ ThousandDevils/Assets/Scripts/WindZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZone : MonoBehaviour
+{
+    [SerializeField] private GameObject _hero;
+    [SerializeField] private float _windSpeed;
+
+    private Rigidbody2D _rb2D;
+    
+    void Start()
+    {
+        _rb2D= _hero.GetComponent<Rigidbody2D>(); 
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        WindZones();
+    }
+
+    private void WindZones()
+    {
+      _rb2D.AddForce(transform.right * _windSpeed);
+    }
+}
