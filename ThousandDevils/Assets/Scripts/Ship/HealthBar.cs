3f54c3600d8d344c72fade335d2bf52fb010091d
degonismi <43546985+degonismi@users.noreply.github.com>
2019-04-09 00:47:45 +0300

    03
    Начал переделывать на базе ивеннтов

---
+++ ThousandDevils/Assets/Scripts/Ship/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Ship
+{
+    public class HealthBar : MonoBehaviour
+    {
+        public ShipHealth Health;
+        public Slider Slider;
+
+        void Start()
+        {
+            Slider.maxValue = Health.Health;
+            Slider.value = Health.Health;
+        }
+
+        public void HealthUpdate()
+        {
+            Slider.value = Health.Health;
+        }
+    }
+}
--- ThousandDevils/Assets/Scripts/Ship/ShipHealth.cs
+++ ThousandDevils/Assets/Scripts/Ship/ShipHealth.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Ship
@@ -6,16 +7,14 @@ namespace Assets.Scripts.Ship
     public class ShipHealth : MonoBehaviour
     {
         [SerializeField] private int _health;
-        private Slider _healthBar;
         
         public int Health => _health;
 
-        void Start()
+        public UnityEvent OnHealthChange;
+
+        void Awake()
         {
             _health = 200;
-            _healthBar = this.gameObject.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Slider>();
-            _healthBar.maxValue = _health;
-            _healthBar.value = _health;
         }
 
         public void DecreaseHealth(uint healthToDecrease)
@@ -23,7 +22,8 @@ namespace Assets.Scripts.Ship
             var remainingHealth = Health - (int) healthToDecrease;
 
             _health = remainingHealth <= 0 ? 0 : remainingHealth;
-            _healthBar.value = _health;
+            if (OnHealthChange != null)
+                OnHealthChange.Invoke();
         }
     }
 }
