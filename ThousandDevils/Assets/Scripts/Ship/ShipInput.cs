9123311829886f36ee88980fd7ac0e65ff1d1a0b
Oleg Korneev <waterpoleg@gmail.com>
2019-05-17 21:08:46 +0300

    010: add shooting accuracy & some tuning to shooting directions

--- ThousandDevils/Assets/Scripts/Ship/ShipInput.cs
+++ ThousandDevils/Assets/Scripts/Ship/ShipInput.cs
@@ -4,15 +4,23 @@ namespace Assets.Scripts.Ship
 {
     public class ShipInput : MonoBehaviour
     {
-        [SerializeField] private GameObject _sheep;
+        [SerializeField] private GameObject _ship;
 
         void Update()
         {
-            var shipMovementController = _sheep.GetComponent<ShipMovementController>();
-            var shipShootController = _sheep.GetComponent<ShipShootController>();
+            var shipMovementController = _ship.GetComponent<ShipMovementController>();
+            var shipShootController = _ship.GetComponent<ShipShootController>();
+            
+            if (Input.GetKey(KeyCode.Space))
+            {
+                shipShootController.IncreaseShootingAccuracy();
+            }
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
                 shipShootController.Shoot();
+                shipShootController.ResetShootingAccuracy();
+            }
 
             if (Input.GetKeyDown(KeyCode.E))
             {
--- ThousandDevils/Assets/Scripts/Ship/ShipShootController.cs
+++ ThousandDevils/Assets/Scripts/Ship/ShipShootController.cs
@@ -1,12 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Assets.Scripts.ObjectPoolingManager;
 using UnityEngine;
+using System.Linq;
 
 namespace Assets.Scripts.Ship
 {
     public class ShipShootController : MonoBehaviour
     {
         public List<GameObject> Cannons;
+
+        private float _shootingAccuracy;
+
+        [SerializeField] int _minShootingAccuracy = 0;
+        [SerializeField] int _maxShootingAccuracy = 100;
+
+        [SerializeField] int _aimingSpeed = 30;
+
+        private Quaternion[] _cannonRotations;
+
+        void Awake()
+        {
+            // remember initial cannon angles
+            _cannonRotations = (from cannon in Cannons select cannon.transform.localRotation).ToArray();
+        }
 
         public void Shoot()
         {
@@ -19,6 +36,60 @@ namespace Assets.Scripts.Ship
                 PoolManager.GetInstance().PlacePooledObject("CannonBall", cannon.transform.position, cannon.transform.rotation);
 
             charger.StartCharge();
+        }
+
+        public void IncreaseShootingAccuracy()
+        {
+            _shootingAccuracy += _aimingSpeed * Time.deltaTime;
+            _shootingAccuracy = Mathf.Clamp(_shootingAccuracy, _minShootingAccuracy, _maxShootingAccuracy);
+
+            for (int i = 0; i < Cannons.Count; i++)
+            {
+                var shootingDirection = Quaternion.Euler(Cannons[i].transform.localRotation.eulerAngles.x,
+                    Cannons[i].transform.localRotation.eulerAngles.y, ApplyAccuracy(i));
+
+                Cannons[i].transform.localRotation = shootingDirection;
+            }
+
+            Debug.Log("Shooting Accuracy = " + _shootingAccuracy);
+        }
+
+        public void ResetShootingAccuracy()
+        {
+            _shootingAccuracy = _minShootingAccuracy;
+
+            for (int i = 0; i < Cannons.Count; i++)
+            {
+                Cannons[i].transform.localRotation = _cannonRotations[i];
+            }
+        }
+
+        float ApplyAccuracy(int cannonNumber)
+        {
+            var angle = _cannonRotations[cannonNumber].eulerAngles.z;
+
+            // always between -180 and 180
+            while (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            while (angle < -180)
+            {
+                angle += 360;
+            }
+
+            var sign = Mathf.Sign(angle);
+
+            var minAccuracyAngle = angle;
+            var maxAccuracyAngle = sign * 90;
+            // always between 0 and 1
+            var normalizedAccuracy = (_shootingAccuracy - _minShootingAccuracy) / (_maxShootingAccuracy - _minShootingAccuracy);
+            var newAngle = Mathf.Lerp(minAccuracyAngle, maxAccuracyAngle, normalizedAccuracy);
+
+            Debug.Log("Old Angle = " + angle + " New angle = " + newAngle);
+
+            return newAngle;
         }
     }
 }
